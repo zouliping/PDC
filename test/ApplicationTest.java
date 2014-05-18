7b3604af5014b0aa350f84232bd16d0eb67f32ad
zouliping <zouliping007@163.com>
2014-05-18 20:40:39 +0800

    试图编写测试用例，官方文档所给出的方法均不存在，未知

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,32 +1,47 @@
 import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.contentType;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
 
 import org.junit.Test;
 
 import play.mvc.Content;
-
+import play.mvc.Result;
 
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ * 
+ * Simple (JUnit) tests that can call all parts of a play app. If you are
+ * interested in mocking a whole application, see the wiki for more details.
+ * 
+ */
 public class ApplicationTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
+	@Test
+	public void simpleCheck() {
+		int a = 1 + 1;
+		assertThat(a).isEqualTo(2);
+	}
 
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
-    }
+	@Test
+	public void renderTemplate() {
+		Content html = views.html.index
+				.render("Your new application is ready.");
+		assertThat(contentType(html)).isEqualTo("text/html");
+		assertThat(contentAsString(html)).contains(
+				"Your new application is ready.");
+	}
 
+	@Test
+	public void homeTest() {
+		Result result = callAction(controllers.routes.ref.Application.index());
+		assertThat(status(result)).isEqualTo(OK);
+		assertThat(contentType(result)).isEqualTo("text/plain");
+		assertThat(charset(result)).isEqualTo("utf-8");
+		assertThat(contentAsString(result)).contains("hello pdc");
+	}
 
+	@Test
+	public void loginTest() {
+		Result result = route(fakeRequest(GET, "/onto/all"));
+		assertThat(status(result)).isEqualTo(OK);
+	}
 }
